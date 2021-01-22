a22ae1d2261e1b64e0deece6546c1ed28673977c
sina <sina.devel@gmail.com>
2021-01-22 17:23:56 +0330

    removed Bowl to Dices

--- main.go
+++ main.go
@@ -73,7 +73,7 @@ func main() {
 	})
 
 	b.Handle("/dice", func(m *tb.Message) {
-		dices := []*tb.Dice{tb.Cube, tb.Dart, tb.Ball, tb.Goal, tb.Dice{Type: "🎳"}}
+		dices := []*tb.Dice{tb.Cube, tb.Dart, tb.Ball, tb.Goal}
 		rnd := rand.New(rand.NewSource(time.Now().Unix()))
 		b.Reply(m, dices[rnd.Intn(len(dices))])
 	})
