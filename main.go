4b71e6b8883d1da219e144dec5e1f81d32970220
sina <sina.devel@gmail.com>
2021-02-28 11:45:54 +0000

    fix bot username

--- main.go
+++ main.go
@@ -79,7 +79,7 @@ func main() {
 	})
 
 	b.Handle("/tofa", func(m *tb.Message) {
-                pat := regexp.MustCompile(`^/tofa(@gotelegram_bot){0,1}(.*)`)
+                pat := regexp.MustCompile(`^/tofa(@gotelegeam_bot){0,1}(.*)`)
                 text := pat.ReplaceAllString(m.Text, "$2")
 		result, err := gt.Translate(text, "auto", "fa")
 		if err != nil {
@@ -90,7 +90,7 @@ func main() {
 	})
 
 	b.Handle("/toen", func(m *tb.Message) {
-                pat := regexp.MustCompile(`^/tofa(@gotelegram_bot){0,1}(.*)`)
+                pat := regexp.MustCompile(`^/tofa(@gotelegeam_bot){0,1}(.*)`)
                 text := pat.ReplaceAllString(m.Text, "$2")
                 result, err := gt.Translate(text, "auto", "en")
 		if err != nil {
