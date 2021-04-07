bc517a75ff3912c190e737a30b7c177e02346505
sina safari <sina.devel@gmail.com>
2021-04-07 13:48:15 +0430

    change webhook to longPoller

--- main.go
+++ main.go
@@ -13,20 +13,13 @@ import (
 )
 
 func main() {
-	var (
-		port      = os.Getenv("PORT")       // sets automatically
-		publicURL = os.Getenv("PUBLIC_URL") // you must add it to your config vars
-		token     = os.Getenv("TOKEN")      // you must add it to your config vars
-	)
-
-	webhook := &tb.Webhook{
-		Listen:   ":" + port,
-		Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
-	}
+	token := os.Getenv("TOKEN")
 
 	pref := tb.Settings{
 		Token:  token,
-		Poller: webhook,
+		Poller: &tb.LongPoller{
+			Timeout: 10 * time.Second,
+		},
 	}
 
 	b, err := tb.NewBot(pref)
