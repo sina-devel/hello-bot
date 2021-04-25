2a06f0ba0f5dc01c696cd947c404b66e71c4219b
sina safari <sina.devel@gmail.com>
2021-04-25 14:28:44 +0000

    Update translategooglefree.go

--- translategooglefree/translategooglefree.go
+++ translategooglefree/translategooglefree.go
@@ -25,7 +25,7 @@ type Result struct {
 func Translate(source, sourceLang, targetLang string) (*Result, error) {
 	var result Result
 
-	req, err := http.NewRequest("GET", "https://translate.google.com/translate_a/single?client=at&dt=t&dt=ld&dt=qca&dt=rm&dt=bd&dj=1&hl=es-ES&ie=UTF-8&oe=UTF-8&inputm=2&otf=2&iid=1dd3b944-fa62-4b55-b330-74909a99969e&dt=t", nil)
+	req, err := http.NewRequest("GET", "https://translate.google.com/translate_a/single?client=at&dt=t&dt=ld&dt=qca&dt=rm&dt=bd&dj=1&hl=es-ES&ie=UTF-8&oe=UTF-8&inputm=2&otf=2", nil)
 	if err != nil {
 		return nil, err
 	}
